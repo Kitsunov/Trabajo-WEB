8d1d94798f1048c2c81dc7e5874dc99d3324ca58
Kitsunov <nova9gp@gmail.com>
2021-10-22 09:12:15 -0500

    Cambio Segundo

---
+++ procesar.php
@@ -0,0 +1,12 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$direccion = $_POST['direccion'];
+$usuario = $_POST['usuario'];
+
+echo "Recibimos... <br>";
+echo "Nombre: ".$nombre."<br>";
+echo "Apellido: ".$apellido."<br>";
+echo "Direccion: ".$direccion."<br>";
+echo "Nombre de usuario: ".$usuario."<br>";
+?>
